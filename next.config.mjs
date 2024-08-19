20f5d8503490fa86da47c7167c063df370178f90
jadj <jakub.jadczyk@innsoft.pl>
2024-08-19 13:21:15 +0200

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,13 @@ const nextConfig = {
         return config
       },
       transpilePackages: ['next-mdx-remote'],
-      // basePath: "/tall_guy",
+      basePath: "/tall_guy",
       output: "export",
       images: {
         unoptimized: true,
+      },
+      experimental: {
+        appDir: true,
       },
 };
 
